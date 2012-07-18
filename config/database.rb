8367c40b40e4a7b7352e4ac7a8974558687f465c
Chris Kowalik <chris@nu7hat.ch>
2012-07-18 15:34:27 +0200

    fixed db conf

--- config/database.rb
+++ config/database.rb
@@ -3,6 +3,7 @@ require 'logger'
 
 if !!ENV['CLEARDB_DATABASE_URL']
   ActiveRecord::Base.establish_connection(ENV['CLEARDB_DATABASE_URL'])
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
 else
   if RACK_ENV != 'production'
     ENV['DATABASE_NAME'] += "_#{RACK_ENV}"
@@ -15,7 +16,7 @@ else
     :username => ENV['DATABASE_USER'],
     :password => ENV['DATABASE_PASS']
   })
-end
 
-db_log_file = File.expand_path('../../log/database.log', __FILE__)
-ActiveRecord::Base.logger = Logger.new(File.open(db_log_file, 'a'))
+  db_log_file = File.expand_path('../../log/database.log', __FILE__)
+  ActiveRecord::Base.logger = Logger.new(File.open(db_log_file, 'a'))
+end
