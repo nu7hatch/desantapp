d61879346fc4b10186c61351660350c37c96ed1e
Chris Kowalik <chris@nu7hat.ch>
2012-07-18 15:20:48 +0200

    cleardb database setup

--- config/database.rb
+++ config/database.rb
@@ -1,17 +1,21 @@
 require 'active_record'
 require 'logger'
 
-if RACK_ENV != 'production'
-  ENV['DATABASE_NAME'] += "_#{RACK_ENV}"
-end
+if !!ENV['CLEARDB_DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['CLEARDB_DATABASE_URL'])
+else
+  if RACK_ENV != 'production'
+    ENV['DATABASE_NAME'] += "_#{RACK_ENV}"
+  end
 
-ActiveRecord::Base.establish_connection({ 
-  :adapter  => ENV['DATABASE_ADAPTER'],
-  :database => ENV['DATABASE_NAME'],
-  :host     => ENV['DATABASE_HOST'],
-  :username => ENV['DATABASE_USER'],
-  :password => ENV['DATABASE_PASS']
-})
+  ActiveRecord::Base.establish_connection({ 
+    :adapter  => ENV['DATABASE_ADAPTER'],
+    :database => ENV['DATABASE_NAME'],
+    :host     => ENV['DATABASE_HOST'],
+    :username => ENV['DATABASE_USER'],
+    :password => ENV['DATABASE_PASS']
+  })
+end
 
 db_log_file = File.expand_path('../../log/database.log', __FILE__)
 ActiveRecord::Base.logger = Logger.new(File.open(db_log_file, 'a'))
